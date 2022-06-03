74bd03cbdc63f8968343ea08fe499980f238113f
The-Great1 <jagreat13@gmail.com>
2022-06-03 03:43:19 -0700

    created main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int print_dec(int n);
+int print_char(char c);
+int print_string(char *s);
+int _printf(const char *format, ...);
+
+
+
+
+
+#endif
